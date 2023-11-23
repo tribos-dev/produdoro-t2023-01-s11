7b87e2e8d9872c29cbad00a2e8311dc3a964b2a3
Juliana Azevedo <140960908+ajulieazvdo@users.noreply.github.com>
2023-11-23 14:31:25 -0300

    implementa testes unitarios

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/TarefaAPI.java
@@ -21,6 +21,5 @@ public interface TarefaAPI {
 
     @DeleteMapping("/{idTarefa}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    void deletaTarefa(@RequestHeader(name = "Authorization",required = true) String token,
-    @PathVariable UUID idTarefa);
+    void deletaTarefa(@RequestHeader(name = "Authorization",required = true) String token, @PathVariable UUID idTarefa);
 }
